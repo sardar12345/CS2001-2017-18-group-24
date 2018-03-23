60515467060c2e5944ae002f0b91deb949221977
sardar12345 <34307773+sardar12345@users.noreply.github.com>
2018-03-23 19:54:47 +0000

    Update DirectionFinder.java

--- Initial Code/DirectionFinder.java
+++ Initial Code/DirectionFinder.java
@@ -5,4 +5,11 @@ public class DirectionFinder {
     private String origin;//where the user wants the start point of the journey
     private String destination;// the destination the user inputs on the map screen
 
-}
+    public DirectionFinder(DirectionFinderListener listener, String origin, String destination)
+    {
+        this.listener = listener;
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+
