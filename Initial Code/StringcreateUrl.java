8d6f4d1fd743bb078788e1ffb7feb6843911d1c2
sardar12345 <34307773+sardar12345@users.noreply.github.com>
2018-03-23 19:59:54 +0000

    Add files via upload

---
+++ Initial Code/StringcreateUrl.java
@@ -0,0 +1,6 @@
+ private String createUrl() throws UnsupportedEncodingException {
+        String urlOrigin = URLEncoder.encode(origin, "utf-8");
+        String urlDestination = URLEncoder.encode(destination, "utf-8");
+
+        return DIRECTION_URL_API + "origin=" + urlOrigin + "&destination=" + urlDestination + "&key=" + GOOGLE_API_KEY;
+    }
