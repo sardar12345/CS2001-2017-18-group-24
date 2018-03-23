bdae784db3c3a747826aecb4b3c54207d3a05e70
sardar12345 <34307773+sardar12345@users.noreply.github.com>
2018-03-23 19:51:02 +0000

    Update DirectionFinder.java

--- Initial Code/DirectionFinder.java
+++ Initial Code/DirectionFinder.java
@@ -2,7 +2,7 @@ public class DirectionFinder {
     private static final String DIRECTION_URL_API = "https://maps.googleapis.com/maps/api/directions/json?"; //
     private static final String GOOGLE_API_KEY = "AIzaSyDnwLF2-WfK8cVZt9OoDYJ9Y8kspXhEHfI";
     private DirectionFinderListener listener; 
-    private String origin;
+    private String origin;//where the user wants the start point of the journey
     private String destination;// the destination the user inputs on the map screen
 
 }
