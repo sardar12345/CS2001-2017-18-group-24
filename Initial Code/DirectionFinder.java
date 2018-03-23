9b6e3bd01c46eff07d23a17c304fdbbc51c2623f
sardar12345 <34307773+sardar12345@users.noreply.github.com>
2018-03-23 19:46:35 +0000

    Add files via upload
    this is code for listening for string inputs (origin and destination) which will be directed to google maps api

---
+++ Initial Code/DirectionFinder.java
@@ -0,0 +1,13 @@
+public class DirectionFinder {
+    private static final String DIRECTION_URL_API = "https://maps.googleapis.com/maps/api/directions/json?"; //
+    private static final String GOOGLE_API_KEY = "AIzaSyDnwLF2-WfK8cVZt9OoDYJ9Y8kspXhEHfI";
+    private DirectionFinderListener listener; // listener textbox in the map screen
+    private String origin;//where the user wants the start point of the journey
+    private String destination;// the destination the user inputs on the map screen
+
+    public DirectionFinder(DirectionFinderListener listener, String origin, String destination)// listens fir the string and destination string
+    {
+        this.listener = listener;
+        this.origin = origin;
+        this.destination = destination;
+    }
