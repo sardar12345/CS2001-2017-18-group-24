da3f2e63f73617f0e64bf23bf2b3712cfa582aca
Chris Llewellyn <christerface@gmail.com>
2018-03-27 11:03:17 +0100

    Added log in validation

--- app/src/main/java/com/example/rbaga/rideshare/LogIn.java
+++ app/src/main/java/com/example/rbaga/rideshare/LogIn.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.text.TextUtils;
+
+
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.tasks.OnCompleteListener;
 
 import com.example.rbaga.rideshare.R;
 
@@ -20,13 +25,25 @@ public class LogIn extends AppCompatActivity implements View.OnClickListener {
 
     //declare Firebase mAuth
     private FirebaseAuth mAuth;
+    private EditText textEmail;
+    private EditText textPassword;
+    private String email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in);
 
+        mAuth = FirebaseAuth.getInstance();
+
         findViewById(R.id.signUpButton).setOnClickListener(this);
+
+        textEmail = findViewById(R.id.textEmail);
+        textPassword = findViewById(R.id.textPassword);
+
+        email = textEmail.getText().toString();
+        password = textPassword.getText().toString();
 
     }
 
@@ -39,6 +56,33 @@ public class LogIn extends AppCompatActivity implements View.OnClickListener {
                 break;
         }
 
+    }
+
+    private void logIn(String email, String password) {
+
+
+    }
+
+    public boolean validateLogIn() {
+        boolean valid = true;
+
+        String email = textEmail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            textEmail.setError("Required.");
+            valid = false;
+        } else {
+            textEmail.setError(null);
+        }
+
+        String password = textPassword.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            textPassword.setError("Required.");
+            valid = false;
+        } else {
+            textPassword.setError(null);
+        }
+
+        return valid;
     }
 
 }
