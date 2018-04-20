60bf1e900a505756bab4cc58a1df207964acf474
donkaluarachchi <nisura12345@gmail.com>
2018-04-20 22:01:38 +0100

    History Class

--- app/src/main/java/History.java
+++ app/src/main/java/History.java
@@ -1,61 +1,35 @@
-public class History {
+public class History extends AppCompatActivity {
 
-//        private String customerOrDriver, userId;
-//
-//        private View HistoryView;
-//        private View.Adapter HistoryAdapter;
-//        private View.LayoutManager HistoryLayoutManager;
-//
-//        private TextView Balance;
-//
-//        private Double Balance = 0.0;
-//
-//        private Button Payout;
-//
-//        private EditText PayoutEmail;
-//
-//        @Override
-//        protected void onCreate(Bundle Ins
-//
-//
-//        Balance = findViewById(id.balance);
-//        Payout = findViewById(id.payout);
-//        PayoutEmail = findViewById(id.payoutEmail);
-//
-//        HistoryView = findViewById(id.historyView);
-//        HistoryView.setNestedScrollingEnabled(false);
-//        HistoryView.setHasFixedSize(true);
-//        HistoryLay = new LinearLayoutManager(HistoryAct.this);
-//        HistoryView.setLayoutManager(HistoryLay);
-//        HistoryAdapter = new HistoryAdapter(getDataSetHistory(), HistoryAct.this);
-//        HistoryView.setAdapter(HistoryAdapter);
-//
-//
-//    }
-//
-//        mPayout.setOnClickListener(new View.OnClickListener() {
-//        @Override
-//        public void onClick(View view) {
-//            payoutRequest();
-//        }
-//    });
-//}
-//
-//
-//
-//
-//
-//    private String getDate(Long time) {
-//        Calendar cal = Calendar.getInstance(Locale.getDefault());
-//        cal.setTimeInMillis(time*1000);
-//        String date = DateFormat.format("MM-dd-yyyy hh:mm", cal).toString();
-//        return date;
-//    }
-//
-//    private ArrayList resultsHistory = new ArrayList<HistoryObject>();
-//    private ArrayList<HistoryObject> getDataSetHistory() {
-//        return resultsHistory;
-//    }
+        private String customerOrDriver, userId;
 
+        private RecyclerView mHistoryRecyclerView;
+
+        private RecyclerView.Adapter mHistoryAdapter;
+
+        private RecyclerView.LayoutManager mHistoryLayoutManager;
+
+
+        protected void onCreate(Bundle savedInstanceState) {
+
+            super.onCreate(savedInstanceState);
+
+            setContentView(R.layout.activity_history);
+
+            HisRecyclerView = (RecyclerView) findViewById(R.id.historyRecyclerView);
+
+            HisRecyclerView.setNestedScrollingEnabled(false);
+
+            HisRecyclerView.setHasFixedSize(true);
+
+            HisLayoutManager = new LinearLayoutManager(HistoryActivity.this);
+
+            HisRecyclerView.setLayoutManager(HisLayoutManager);
+
+            HisAdapter = new HistoryAdapter(getDataSetHistory(), HistoryActivity.this);
+
+            HisRecyclerView.setAdapter(mHistoryAdapter);
+
+
+        }
 
 }
