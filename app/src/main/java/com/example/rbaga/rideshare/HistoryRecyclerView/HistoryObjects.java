5bd21b6db581216ab05805ddcccc30811a6422d4
donkaluarachchi <nisura12345@gmail.com>
2018-04-20 23:04:49 +0100

    Added Method to History Objects

--- app/src/main/java/com/example/rbaga/rideshare/HistoryRecyclerView/HistoryObjects.java
+++ app/src/main/java/com/example/rbaga/rideshare/HistoryRecyclerView/HistoryObjects.java
@@ -1,4 +1,15 @@
 package com.example.rbaga.rideshare.HistoryRecyclerView;
 
 public class HistoryObjects {
+
+        private String rideId;
+        private String time;
+
+        public HistoryObject(String rideId, String time){
+            this.rideId = rideId;
+            this.time = time;
+        }
+
+
+
 }
